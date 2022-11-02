3fd7df6a10a08c87aad382c876c21528f652aee2
Yi-Wen LIN <bongiovi015@gmail.com>
2022-11-02 09:06:14 +0000

    add lesson 2 triangle with colors

---
+++ alfrid/02_triangleWithColor/src/index.js
@@ -0,0 +1,54 @@
+import { GL, Mesh, GLShader } from "alfrid";
+const canvas = document.createElement("canvas");
+document.body.appendChild(canvas);
+GL.init(canvas);
+
+// shaders
+import vs from "shaders/basic.vert";
+import fs from "shaders/triangle.frag";
+
+// resize
+const resize = () => {
+  const { innerWidth, innerHeight } = window;
+  GL.setSize(innerWidth, innerHeight);
+};
+
+resize();
+window.addEventListener("resize", resize);
+
+// clear
+GL.clear(0, 0, 0, 1);
+
+// setup vertices
+const r = 0.5;
+const points = [
+  [-r, -r, 0],
+  [r, -r, 0],
+  [0, r, 0],
+];
+
+// setup colors
+const t = 0;
+const colors = [
+  [1, t, t],
+  [t, 1, t],
+  [t, t, 1],
+];
+
+// setup indices
+const indices = [0, 1, 2];
+
+// create mesh
+const mesh = new Mesh()
+  .bufferData(points, "aPosition")
+  .bufferData(colors, "aColor")
+  .bufferIndex(indices);
+
+// create shader
+const shader = new GLShader(vs, fs);
+
+// bind shader
+shader.bind();
+
+// draw mesh
+GL.draw(mesh);
